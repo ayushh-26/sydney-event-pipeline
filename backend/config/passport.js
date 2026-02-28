83b6ce10a959248cbc1bcb75e7ae5cdcb6dcc2e8
ayushh-26 <ayush26sehrawat@gmail.com>
2026-03-01 04:30:50 +0530

    fix: production auth redirect

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,10 +2,10 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
 module.exports = function(passport) {
-  passport.use(new GoogleStrategy({
+ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/auth/google/callback"
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
